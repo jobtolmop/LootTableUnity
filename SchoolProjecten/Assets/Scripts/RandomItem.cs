cfb530bf71362538b4806f45d7c3cb87b2a3d9b9
Everardus Tol <43673344+jobtolmop@users.noreply.github.com>
2020-11-11 06:47:01 +0100

    Initial commit

---
+++ SchoolProjecten/Assets/Scripts/RandomItem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomItem : MonoBehaviour
+{
+    public List<GameObject> colors;
+
+    public int[] table =
+    {
+        60, //common
+        25, //rare
+        10, //epic
+        5 //legendary
+    };
+
+    public int total;
+    public int randomNumber;
+
+    private void Start()
+    {
+        foreach(var item in table)
+        {
+            total += item;
+        }
+
+        randomNumber = Random.Range(0, total);
+
+        for (int i = 0; i < table.Length; i++)
+        {
+            if (randomNumber <= table[i])
+            {
+                colors[i].SetActive(true);
+                return;
+            }
+            else
+            {
+                randomNumber -= table[i];
+            }
+        }
+    }
+
+
+
+}
